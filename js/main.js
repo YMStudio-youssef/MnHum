af5982eb8aa3d92c53dea256b4a31426ab0e5a50
YMStudio-youssef <133716171+YMStudio-youssef@users.noreply.github.com>
2024-02-06 02:11:37 +0300

    Commit 02
    complete some website pages with the style

--- js/main.js
+++ js/main.js
@@ -5,4 +5,20 @@ $(document).ready(function() {
     $(window).resize(function (){
         $('.responsive-menu').hide()
     })
+      // Initial setup
+    $('.tab-button:first').addClass('active');
+    $('.tab-content:first').show();
+
+    // Tab click event
+    $('.tab-button').click(function() {
+        var tabId = $(this).data('tab');
+
+        // Remove 'active' class from all buttons and hide all tab contents
+        $('.tab-button').removeClass('active');
+        $('.tab-content').hide();
+
+        // Add 'active' class to the clicked button and show the corresponding tab content
+        $(this).addClass('active');
+        $('#' + tabId).show();
+    });
 });
